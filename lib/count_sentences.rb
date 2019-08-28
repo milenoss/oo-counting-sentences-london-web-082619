99817e2421d718b566b3ef59d2651527d034014d
github username <github email address>
2019-08-28 18:17:27 +0100

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -7,14 +7,14 @@ class String
   end
 
   def question?
-    self.end_with("?") 
+    self.end_with?("?") 
   end
 
   def exclamation?
-  self.end_with("!")
+  self.end_with?("!")
   end
 
   def count_sentences
-
+self.split(/[.?!]+/).count
   end
 end
