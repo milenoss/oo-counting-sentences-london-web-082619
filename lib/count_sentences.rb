cf87c122b08f4f50eafe229f8d99777e384ebffb
github username <github email address>
2019-08-28 17:59:28 +0100

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,11 +3,13 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    if self.end_with("?")
+    else
+      puts false
   end
 
   def exclamation?
